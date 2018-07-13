5bfa4b8c0d66d273f493630ea42746af1c9d6572
Morales Franco <franco.e.morales@gmail.com>
2018-07-13 00:18:42 -0300

    Integración con SignalR

---
+++ CertificationTraining/TestSite/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+namespace TestSite
+{
+    //Generamos clase Startup para utilizar SignalR
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
